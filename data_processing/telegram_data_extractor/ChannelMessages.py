d4dd14068d81866d2be97687b287ccc4088967f7
amitchew <amitchew@gmail.com>
2024-01-31 16:08:14 +0300

    feat: extract,clean and convert tg data to csv

--- data_processing/telegram_data_extractor/ChannelMessages.py
+++ data_processing/telegram_data_extractor/ChannelMessages.py
@@ -9,8 +9,8 @@ from telethon.tl.functions.messages import (GetHistoryRequest)
 from telethon.tl.types import (
     PeerChannel
 )
-
-
+import re
+import csv
 # some functions to parse json date
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, o):
@@ -38,6 +38,12 @@ username = config['Telegram']['username']
 
 # Create the client and connect
 client = TelegramClient(username, api_id, api_hash)
+link_pattern = re.compile(r'https?:\/\/\S+')
+other_symbol_pattern = re.compile(r'[^\w\s#]')  # Remove non-alphanumeric characters except spaces
+multiple_spaces_pattern = re.compile(r'\s{2,}')  # Remove multiple consecutive spaces
+
+# Remove non-alphabetical characters
+
 
 async def main(phone):
     await client.start()
@@ -94,58 +100,77 @@ async def main(phone):
 
         ## Tenx##
         
-        messages = history.messages
-        for message in messages:
-            message_dict = {
-                "id": message.id,
-                "date": message.date,
-                "message": message.message,
-                "label":"Retail"
-                }
-            all_messages.append(message_dict)
-        offset_id = messages[-1].id
-        total_messages = len(all_messages)
-        if total_count_limit != 0 and total_messages >= total_count_limit:
-            break
-        
-        ## Tenx##
-        ##new##
-        # all_messages = []
-
         # messages = history.messages
-
-        # # Extract channel ID from the first message
-        # channel_id = messages[0].peer_id.channel_id
-
         # for message in messages:
         #     message_dict = {
         #         "id": message.id,
-        #         "date": message.date.isoformat(),
+        #         "date": message.date,
         #         "message": message.message,
-        #         "label": "Real State"
-        #     }
+        #         "label":"Retail"
+        #         }
         #     all_messages.append(message_dict)
+        # offset_id = messages[-1].id
+        # total_messages = len(all_messages)
+        # if total_count_limit != 0 and total_messages >= total_count_limit:
+        #     break
+        
+        ## Tenx##
+        ##Extract messages##
+        all_messages = []
 
-        #     # Break loop if total_messages limit is reached
-        #     total_messages = len(all_messages)
-        #     if total_count_limit != 0 and total_messages >= total_count_limit:
-        #         break
+        messages = history.messages
 
-        # # Construct the final output
-        # output = {
-        #     "name": "Beton: Rentals 🇪🇹",  # Replace with the actual name if available
-        #     "id": channel_id,
-        #     "messages": all_messages
-        # }
+        # Extract channel ID from the first message
+        channel_id = messages[0].peer_id.channel_id
+        
+        for message in messages:
+            if message.message and message.message.strip():
 
-        # # Convert the output to JSON and write it to a file
-        # with open('የኮንስትራክሽን_እቃዎች_አቅራቢ_construction_Materials_supplier.json', 'w', encoding='utf-8') as outfile:
+                clean_message = message.message.replace('\n', '')
+                clean_message = link_pattern.sub('', clean_message)
+                clean_message = other_symbol_pattern.sub('', clean_message)
+                clean_message = multiple_spaces_pattern.sub(' ', clean_message)
+
+
+                message_dict = {
+                    "id": message.id,
+                    "date": message.date.isoformat(),
+                    "message": clean_message,
+                    "label": "Telecom_ad"
+                }
+                all_messages.append(message_dict)
+
+            # Break loop if total_messages limit is reached
+                total_messages = len(all_messages)
+                if total_count_limit != 0 and total_messages >= total_count_limit:
+                    break
+
+        # Construct the final output
+        output = {
+            "name": "Tikvah",  # Replace with the actual name if available
+            "channel_id": channel_id,
+            "messages": all_messages
+        }
+
+        # Convert the output to JSON and write it to a file
+        # with open('Tikvah.json', 'w', encoding='utf-8') as outfile:
         #     json.dump(output, outfile, ensure_ascii=False, indent=2)
 
-        ##new##
+        ##Extract messages##
 
-    with open('Orkid_Design_&_Pillow.json', 'w', encoding='utf-8') as outfile:
-        json.dump(all_messages, outfile, cls=DateTimeEncoder, ensure_ascii=False, indent=2)
+        ##
+        # Write data to CSV
+        with open('Tikvah_data.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Message', 'Label'])  # Write header
+            
+            for message in output['messages']:
+                writer.writerow([message['message'], message['label']])
+        ###
+    # with open('Orkid_Design_&_Pillow.json', 'w', encoding='utf-8') as outfile:
+    #     json.dump(all_messages, outfile, cls=DateTimeEncoder, ensure_ascii=False, indent=2)
+    #    json_data = json.dumps(output, ensure_ascii=False, indent=2)
+
 
 
 with client:
