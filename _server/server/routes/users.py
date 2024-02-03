6069a57364488ca15963f3a39eef414d2f5a32d9
Aaron Gebremaraim <mulabr673@gmail.com>
2024-02-03 08:25:41 +0300

    post

--- _server/server/routes/users.py
+++ _server/server/routes/users.py
@@ -2,16 +2,19 @@ from flask import Blueprint
 from flask import Response, request,jsonify
 
 
-users_bp = Blueprint('users', __name__, url_prefix='/users')
-@users_bp.route('', methods=['GET'])
-def get_all_telegram_users():
-  users = db.session.sca
-  return jsonify(all_users)
+users_bp = Blueprint('/', __name__, url_prefix='/users')
 
-
-@users_bp.route('', method=["POST"])
-def create_user():
-  d = request.json
-  print(d)
-  return Response(status=200)
-  return jsonify(d), 201
+@users_bp.route('/submit_data', method=["POST"])
+def submit_data():
+  try:
+    data = request.json
+    submitted_string = data.get('submitted_string', '')
+    processed_result = process_string(submitted_string)
+    return jsonify({'status': 'success', 'result': processed_result})
+  except Exception as e:
+        # Handle exceptions and return an error response
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+ 
+def process_string(submitted_string):
+    # updated later
+    return submitted_string.upper()
